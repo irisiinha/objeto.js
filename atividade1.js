96dfeade152d80a97513b9cf4fbef131d8a02e0f
irisiinha <irismeiresilvaoliveira@gmail.com>
2025-04-09 16:58:11 +0000

    pronto

--- atividade1.js
+++ atividade1.js
@@ -3,6 +3,10 @@ const endereco ={
     cep:"46500-000",
     estado:'bahia',
     cidade:'macaubas',
-
+    contato:{
+        nome:"iris",
+        telefone:"77999857137",
+        whatsapp:"+55 77999857137"
+    }
 }
 console.log(endereco)

9e62bdd1cefc65652e15f7893237b01487fd4037
irisiinha <irismeiresilvaoliveira@gmail.com>
2025-04-09 16:45:07 +0000

    pronto

---
+++ atividade1.js
@@ -0,0 +1,8 @@
+const endereco ={
+    rua:'varzea verde',
+    cep:"46500-000",
+    estado:'bahia',
+    cidade:'macaubas',
+
+}
+console.log(endereco)
---
+++ exemplo.js
@@ -0,0 +1,8 @@
+const pessoa = {
+    nome:"iris",
+    idade:16,
+    endereco:{
+        rua:'varzea verde',
+        cep:'46500-000'
+    }
+}
